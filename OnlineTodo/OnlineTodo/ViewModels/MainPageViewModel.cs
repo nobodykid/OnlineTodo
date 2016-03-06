94b269392760e325991c14a9a5424f9f1f4ff908
nobodykid <muhamadiqbal@live.co.uk>
2016-03-07 05:24:07 +0700

    successfully connected to Azure Mobile Apps

--- OnlineTodo/OnlineTodo/ViewModels/MainPageViewModel.cs
+++ OnlineTodo/OnlineTodo/ViewModels/MainPageViewModel.cs
@@ -4,11 +4,37 @@ using System.Threading.Tasks;
 using Template10.Mvvm;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
+using OnlineTodo.Models;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.UI.Popups;
 
 namespace OnlineTodo.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public MobileServiceCollection<TodoItem, TodoItem> Items;
+        public IMobileServiceTable<TodoItem> todoTable = App.MobileService.GetTable<TodoItem>();
+
+        public async Task AddNewItem (TodoItem _item)
+        {
+            // Menambahkan Item baru ke dalam list dan database
+
+            await todoTable.InsertAsync(_item);
+            Items.Add(_item);
+                        
+            // also provide offline features
+        }
+
+        public async Task UpdateCheckedItem (TodoItem _item)
+        {
+            // Digunakan untuk update database di mana Item yang di-check
+            // akan memiliki nilai Checked = true
+
+            await todoTable.UpdateAsync(_item);
+            Items.Remove(_item);            
+
+            //await SyncAsync(); // offline sync
+        }        
     }
 }
 
--- OnlineTodo/OnlineTodo/Views/MainPage.xaml.cs
+++ OnlineTodo/OnlineTodo/Views/MainPage.xaml.cs
@@ -1,14 +1,63 @@
 using System;
 using Windows.UI.Xaml.Controls;
+using OnlineTodo.ViewModels;
+using OnlineTodo.Models;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Navigation;
 
 namespace OnlineTodo.Views
 {
     public sealed partial class MainPage : Page
     {
+        public MainPageViewModel vm { get; set; } = new MainPageViewModel();
+
         public MainPage()
         {
             InitializeComponent();
             NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await vm.AddNewItem(new Models.TodoItem
+            {
+                Text = contentTxt.Text,
+                Complete = false
+            });
+
+            await RefreshTodoItems();
+        }
+
+        private async void CheckBox_Checked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            await vm.UpdateCheckedItem(item);
+        }
+
+        private async Task RefreshTodoItems()
+        {            
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems.
+                vm.Items = await vm.todoTable
+                    .Where(todoItem => todoItem.Complete == false)
+                    .ToCollectionAsync();
+
+                ItemList.ItemsSource = vm.Items;
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                await new MessageDialog(e.Message, "Error loading items").ShowAsync();
+            }            
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await RefreshTodoItems();
         }
     }
 }
